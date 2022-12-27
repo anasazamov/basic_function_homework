cbb135b8f1527f5027284103923b1c4c5f5320a8
anasazamov <anasazamov@gmail.com>
2022-12-27 13:59:46 +0500

    basic_func_homework_replay2

--- basic_func09.py
+++ basic_func09.py
@@ -1,8 +1,8 @@
 # Create a function called main.
-def main():
+def main(a):
 # Create function arguments a.
     a=5
     a-=1
 # decrease the value of a to one and return
     return a
-print(main())
+print(main(7))
--- basic_func10.py
+++ basic_func10.py
@@ -1,6 +1,7 @@
 # Create a function called main.
-def main():
+def main(a):
 # Create function arguments a.
     a=-56
 # Return the opposite value of a.
     return -1*a
+print(main(7))
