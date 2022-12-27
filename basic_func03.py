0212a4daf10a2f6e6d0ba10c5e5cab11db0951c6
anasazamov <anasazamov@gmail.com>
2022-12-27 13:57:25 +0500

    basic_func_homework_replay

--- basic_func03.py
+++ basic_func03.py
@@ -1,4 +1,4 @@
 # Create a function called main.
 def main():
 # Return the value "codeschooluz"
-    return "codeschool"
+    return "codeschooluz"
--- basic_func07.py
+++ basic_func07.py
@@ -1,6 +1,7 @@
 # Create a function called main.
-def main():
+def main(a):
 # Create function arguments a.
     a=5
 # Return the value a.
     return a
+print(main(7))
